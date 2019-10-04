e217fea9a6d8ac0f9fd3962e6198435d7eef4e36
JKAnderson <josepha93@gmail.com>
2019-10-04 18:19:38 -0400

    Revert "Add .gitignore, remove pycaches"
    This reverts commit d3dc05fae5a54bc7eff94daa621ec88d70428c29.

--- market/views.py
+++ market/views.py
@@ -1,16 +1,3 @@
 from django.shortcuts import render
-import json
-import requests
 
-def isbn(request, isbn):
-    r = requests.get('https://www.googleapis.com/books/v1/volumes', params={'q': isbn})
-    j = json.loads(r.text)
-    info = j['items'][0]['volumeInfo']
-    
-    context = {
-        'isbn': info['industryIdentifiers'][1]['identifier'],
-        'title': info['title'],
-        'authors': info['authors'][0],
-    }
-    
-    return render(request, 'isbn.html', context=context)
+# Create your views here.
