fd52e3c1b4e58edbaae68137c2113dc3852c608e
JKAnderson <josepha93@gmail.com>
2019-10-16 11:54:06 -0400

    Basic forms for adding listings and requests

--- market/urls.py
+++ market/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('books/',views.BookListView.as_view(), name='books'),
     path('authors/',views.AuthorListView.as_view(), name='authors'),
     path('listings/',views.ListingListView.as_view(), name='listings'),
+    path('listings/add/', views.add_listing_check, name='add-listing-check'),
+    path('listings/add/<str:isbn>/', views.add_listing, name='add-listing'),
     path('bookrequests/',views.BookRequestListView.as_view(), name='bookrequests'),
+    path('bookrequests/add/', views.add_request_check, name='add-request-check'),
+    path('bookrequests/add/<str:isbn>/', views.add_request, name='add-request'),
 ]
--- market/views.py
+++ market/views.py
@@ -1,8 +1,10 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-
-from market.models import Book, Author, Listing, BookRequest
-
+from django.urls import reverse
 from django.views import generic
+
+from . forms import CheckISBNForm, AddListingForm, AddRequestForm
+from . models import Book, Author, Listing, BookRequest
 
 def index(request):
     """View function for home page of site."""
@@ -25,14 +27,91 @@ def index(request):
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
+
 class BookListView(generic.ListView):
     model = Book
+
 
 class AuthorListView(generic.ListView):
     model = Author
 
+
 class ListingListView(generic.ListView):
     model = Listing
 
+
 class BookRequestListView(generic.ListView):
     model = BookRequest
+
+
+def add_listing_check(request):
+    if request.method == 'POST':
+        form = CheckISBNForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect(reverse('add-listing', args=(form.cleaned_data['isbn'],)))
+    
+    else:
+        form = CheckISBNForm()
+        
+    context = {
+        'check_isbn_form': form,
+    }
+    return render(request, 'market/add_listing_check.html', context)
+
+
+def add_listing(request, isbn):
+    if request.method == 'POST':
+        form = AddListingForm(request.POST)
+        if form.is_valid():
+            price = form.cleaned_data['price']
+            condition = form.cleaned_data['condition']
+            comment = form.cleaned_data['comments']
+            
+            book = Book.objects.get(isbn=isbn)
+            listing = Listing(price=price, condition=condition, comment=comment, book=book)
+            listing.save()
+            return HttpResponseRedirect(reverse('listings'))
+    
+    else:
+        form = AddListingForm(initial={'isbn': isbn})
+    
+    context = {
+        'add_listing_form': form,
+    }
+    return render(request, 'market/add_listing.html', context)
+
+def add_request_check(request):
+    if request.method == 'POST':
+        form = CheckISBNForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect(reverse('add-request', args=(form.cleaned_data['isbn'],)))
+    
+    else:
+        form = CheckISBNForm()
+        
+    context = {
+        'check_isbn_form': form,
+    }
+    return render(request, 'market/add_request_check.html', context)
+
+
+def add_request(request, isbn):
+    if request.method == 'POST':
+        form = AddRequestForm(request.POST)
+        if form.is_valid():
+            price = form.cleaned_data['price']
+            condition = form.cleaned_data['condition']
+            comment = form.cleaned_data['comments']
+            
+            book = Book.objects.get(isbn=isbn)
+            req = BookRequest(desired_price=price, desired_condition=condition, comment=comment, book=book)
+            req.save()
+            return HttpResponseRedirect(reverse('bookrequests'))
+    
+    else:
+        form = AddRequestForm(initial={'isbn': isbn})
+    
+    context = {
+        'add_request_form': form,
+    }
+    return render(request, 'market/add_request.html', context)
